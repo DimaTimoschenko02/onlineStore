f87492be305a2177f98962c8b29e0515970c7eb1
Ihor Pastushenko <pastushokk97@gmail.com>
2022-04-05 17:15:55 +0300

    Refactoring

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -1,23 +1,22 @@
-const {Type} = require('../models/models')
-const ApiError = require('../error/ApiError')
+const { Type } = require('../models/models');
 
-
-
-class TypeController{
-    async create(req , res){
-        const {name} = req.body
-        const type = await Type.create({name})
-        return res.send(type)
+class TypeController {
+    async create(req, res){
+      const { name } = req.body;
+      const type = await Type.create({ name });
+      return res.send(type);
     }
+
     async getAll(req , res){
-        const types = await Type.findAll()
-        return res.json(types)
+      const types = await Type.findAll();
+      return res.json(types);
     }
+
     async deleteOne(req, res){
-        const {id} = req.params
-        await Type.destroy({where:{id}})
-        res.json({message: 'ok'}) 
+      const { id } = req.params;
+      await Type.destroy({ where: { id } });
+      res.json({ message: 'ok' });
     }
 }
 
-module.exports = new TypeController()
+module.exports = new TypeController();
